04d8a59bc1e65e698e885f7f0bd69fadea60e291
thines <skwebbies@hotmail.co.uk>
2020-03-15 00:07:31 +0100

    initial commit

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+
+<head>
+	<base href="/admin/">
+	<script src="/dmxAppConnect/dmxAppConnect.js"></script>
+	<meta charset="UTF-8">
+	<title>Untitled Document</title>
+	<script src="/js/jquery-3.4.1.slim.min.js"></script>
+	<link rel="stylesheet" href="/fontawesome4/css/font-awesome.min.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="/bootstrap/4/css/bootstrap.min.css" />
+</head>
+
+<body is="dmx-app" id="index">
+	<script src="/bootstrap/4/js/popper.min.js"></script>
+	<script src="/bootstrap/4/js/bootstrap.min.js"></script>
+</body>
+
+</html>

3698ae15060b98c6535e909387a50ace25b91497
thines <skwebbies@hotmail.co.uk>
2020-03-15 01:09:41 +0100

    adds admin dashboard navbar and styles

---
+++ dmxConnect/modules/Connections/db.php
@@ -0,0 +1,15 @@
+<?php
+// Database Type : "MySQL"
+// Database Adapter : "mysql"
+$exports = <<<'JSON'
+{
+    "name": "db",
+    "module": "dbconnector",
+    "action": "connect",
+    "options": {
+        "server": "mysql",
+        "connectionString": "mysql:host=db;sslverify=false;port=3306;dbname=xchange;user=db_user;password=9aznd6Nz;charset=utf8"
+    }
+}
+JSON;
+?>
